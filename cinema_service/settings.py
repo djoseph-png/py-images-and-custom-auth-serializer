aaed13d609a5d2327f34db35f2313cb6d49912af
Daniel Joseph <djoseph@envisagebr.com>
2025-10-01 04:43:09 +0300

    Enforce LF and trailing newline; ensure EOF newline (settings.py, migration)

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+from django.db.models.utils import make_model_tuple
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
