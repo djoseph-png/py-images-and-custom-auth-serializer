86d118a9534a5bfeb0ccdf02905cf4705af8ba01
Daniel Joseph <djoseph@envisagebr.com>
2025-10-01 04:08:30 +0300

    Expose DRF router with cinema namespace; serve media in DEBUG; fix flake8; add Pillow

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -1,14 +1,14 @@
-from django.urls import path, include
-from django.conf import settings  # importa settings
-
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include, path
 from rest_framework import routers
 
 from cinema.views import (
-    GenreViewSet,
     ActorViewSet,
     CinemaHallViewSet,
-    MovieViewSet,
+    GenreViewSet,
     MovieSessionViewSet,
+    MovieViewSet,
     OrderViewSet,
 )
 
@@ -21,12 +21,22 @@ router.register("movie_sessions", MovieSessionViewSet)
 router.register("orders", OrderViewSet)
 
 urlpatterns = [
+    # expõe as rotas do DRF com namespace "cinema"
     path("", include(("cinema.urls", "cinema"), namespace="cinema")),
+    path("api/cinema/", include(router.urls)),
     path("api/user/", include(("user.urls", "user"), namespace="user")),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
+
     urlpatterns += [
-        path("__debug__/", include(debug_toolbar.urls)),
+        path(
+            "__debug__/",
+            include(debug_toolbar.urls),
+        ),
     ]
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
